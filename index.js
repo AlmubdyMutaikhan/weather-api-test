a064b4fe2c897d8f31acc34787aeeafe994500de
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-11-08 13:11:05 +0600

    temp

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 app.get('/:city', async (req, res) => {
     try {
         const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=d3fe3b9184c85b7b49da9e30b7dbf0de`);
-        const temp = weather.data.main.temp - 273.15;
+        const temp = Math.round(weather.data.main.temp - 273.15);
         res.status(200).send({msg:"ok", temp});
     } catch(err) {
         res.status(400).send({msg:err.message})
