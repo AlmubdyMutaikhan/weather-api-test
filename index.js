1c0fdcebe38174516cf74331a640899233ea6a67
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-11-08 13:00:47 +0600

    ad

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(cors({
     origin : '*'
 }));
 
+
+app.get('/', (req, res) => {
+    res.send("SERVER IS UP!");
+})
 app.get('/:city', async (req, res) => {
     try {
         const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=d3fe3b9184c85b7b49da9e30b7dbf0de`);
@@ -16,6 +20,7 @@ app.get('/:city', async (req, res) => {
         res.status(400).send({msg:err.message})
     }
 })
+
 
 app.listen(PORT, () => {
     console.log('server is running');
